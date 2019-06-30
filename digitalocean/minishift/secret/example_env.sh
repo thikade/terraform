52d905334abc70cfe50d617a3a7b2349edebb9e4
thikade <thomas.hikade@gmail.com>
2019-06-30 18:46:04 +0200

    added example secret

---
+++ digitalocean/minishift/secret/example_env.sh
@@ -0,0 +1,9 @@
+export TF_VAR_do_token=xxx
+
+# used for login to droplet by remote_exec provisioner
+export TF_VAR_pub_key=/path/to/public_key.pub   
+export TF_VAR_priv_key=/path/to/private_key.pub
+
+# This PUB_KEY will be added to droplet (must already be uploaded to DO before use!)
+export TF_VAR_ssh_fingerprint=$(ssh-keygen -E md5 -lf $TF_VAR_pub_key | awk '{print $2}' | cut -d: -f2- )
+
