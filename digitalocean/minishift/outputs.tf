ca66735bc9d22110854ce85ec228d85250774a0f
thikade <thomas.hikade@gmail.com>
2020-04-28 11:53:30 +0200

    TF 0.12 fixes

--- digitalocean/minishift/outputs.tf
+++ digitalocean/minishift/outputs.tf
@@ -1,7 +1,7 @@
 output "minishift_fqdn" {
-  value = "${digitalocean_record.minishift.fqdn}"
+  value = digitalocean_record.minishift.fqdn
 }
 
 output "minishift_ipv4" {
-  value = "${digitalocean_droplet.minishift.ipv4_address}"
+  value = digitalocean_droplet.minishift.ipv4_address
 }
